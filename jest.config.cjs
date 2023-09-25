537c478ed5d609c81b0b3f7a2f21c92ee52936e3
Deivis Marchán <marchandeivis@gmail.com>
2023-09-25 10:47:49 -0400

    Updated

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,4 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['./setupTests.js'],
-    // Otras configuraciones de Jest
-  };
-  
+  testEnvironment: 'jest-environment-jsdom',
+  setupFiles: ['./jest.setup.js']
+}
